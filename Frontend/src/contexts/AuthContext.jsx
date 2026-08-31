70e625b5715c0786d800981abfb5fe6cb47a4563
Rabiya Zahid <zoyakhan95234@gmail.com>
2026-08-31 12:35:08 +0530

    Connect frontend to deployed backend

--- Frontend/src/contexts/AuthContext.jsx
+++ Frontend/src/contexts/AuthContext.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import httpStatus from "http-status";
 export  const   AuthContext=createContext({});
 const   client=axios.create({
-    baseURL:"http://localhost:8000/api/v1/users"
+    baseURL:"https://zoom-clone-backend-zt2g.onrender.com/api/v1/users"
 })
 export  const   AuthProvider=({children})=>{
     const   authContext=useContext(AuthContext);
--- Frontend/src/environment.js
+++ Frontend/src/environment.js
@@ -1,3 +1,3 @@
-const server = "http://localhost:8000";
+const server = "https://zoom-clone-backend-zt2g.onrender.com";
 
 export default server;
